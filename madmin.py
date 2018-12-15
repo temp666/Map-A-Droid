b2a338070546898662289befd0fd1e525901c279
temp <dampf.tasche@gmail.com>
2018-12-15 13:11:02 +0100

    changes

--- madmin.py
+++ madmin.py
@@ -374,8 +374,7 @@ def get_unknows():
     unk = []
     for file in glob.glob("www_hash/unkgym_*.jpg"):
         unkfile = re.search('unkgym_(-?\d+\.?\d+)_(-?\d+\.?\d+)_((?s).*)\.jpg', file)
-        #creationdate = datetime.datetime.fromtimestamp(creation_date(file)).strftime('%Y-%m-%d %H:%M:%S')
-        creationdate = datetime.datetime.fromtimestamp(creation_date(file)).isoformat()
+        creationdate = datetime.datetime.fromtimestamp(creation_date(file)).strftime('%Y-%m-%d %H:%M:%S')
         lat = (unkfile.group(1))
         lon = (unkfile.group(2))
         hashvalue = (unkfile.group(3))
